30114f9e7ab01dcea446871c49b53273388fb6c9
Enzo Satti <enzo.satti.etu@univ-lille.fr>
2021-10-05 22:19:21 +0200

    debut lecture fichier ply

---
+++ src/main/java/lecture/Face.java
@@ -0,0 +1,30 @@
+package lecture;
+
+import java.util.ArrayList;
+
+public class Face {
+	private ArrayList<Point> points;
+	private int nb_points;
+	
+	public Face(ArrayList<Point> p, int np) {
+		this.points = p; this.nb_points = np;
+	}
+
+	public ArrayList<Point> getPoints() {
+		return points;
+	}
+
+	public void setPoints(ArrayList<Point> points) {
+		this.points = points;
+	}
+
+	public int getNb_points() {
+		return nb_points;
+	}
+
+	public void setNb_points(int nb_points) {
+		this.nb_points = nb_points;
+	}
+	
+	
+}
