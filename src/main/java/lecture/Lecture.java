48e1c9b1df6b0c449686c30a56965d5d0993bb03
Enzo Satti <enzo.satti.etu@univ-lille.fr>
2021-10-14 09:48:35 +0200

    separation faces et coordonnees

--- src/main/java/lecture/Lecture.java
+++ src/main/java/lecture/Lecture.java
@@ -5,12 +5,15 @@ import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
 import java.io.PrintWriter;
+import java.util.Scanner;
 
 public class Lecture {
 	public static void lecture(String file) {
 		String line;
 		String entete=""; //pour mettre toute la partie avant les coordonnées des points, sera gérée plus tard
-		String données=""; // en attendant que je fasse la séparation entre points et faces
+		String coordonnees=""; 
+		String faces="";
+		
 		try(BufferedReader br = new BufferedReader(new FileReader(file))) {
 			line = br.readLine();
 			while(!line.split(" ")[0].equals("end_header")) {
@@ -19,22 +22,22 @@ public class Lecture {
 			}
 			ecrire_fichier(entete, "./ressources/entete.txt");
 			line = br.readLine();
-			while (line!=null) {
-				données = données +line+"\n";
+			while (line!=null && !est_ligne_face(line)) {
+				coordonnees = coordonnees +line+"\n";
 				line = br.readLine();
 			}
-			ecrire_fichier(données, "./ressources/coordonnes.txt");
+			ecrire_fichier(coordonnees, "./ressources/coordonnes.txt");
+			while(line!=null) {
+				faces= faces +line+"\n";
+				line = br.readLine();
+			}
+			ecrire_fichier(faces, "./ressources/faces.txt");
 		}catch(IOException e) {
 			System.out.println("Une erreur est survenue");
 			e.printStackTrace();
 		}
 	}
 	
-	public static void stocker_donnees(String[] line, String d) {
-		for(int i =0; i< line.length; i++) {
-			d += line[i];
-		}
-	}
 	
 	public static void ecrire_fichier(String ligne, String fichier_dest) {
 		PrintWriter writer;
@@ -48,7 +51,20 @@ public class Lecture {
 		}
 	}
 	
+	public static boolean est_ligne_face(String ligne) {
+		String[] mots = ligne.split(" ");
+		Scanner scanner = new Scanner(mots[0]);
+		if(scanner.hasNextInt()) {
+		int nb = scanner.nextInt();
+		if(nb==mots.length-1) {
+			return true;
+		}
+		}
+		return false;
+	}
+	
 	public static void main(String [] args) {
 		lecture("./ressources/test.ply");
 	}
 }
+
