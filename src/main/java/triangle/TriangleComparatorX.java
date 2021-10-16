fdefc8e56d0b478ecbf5fcd3e219af88f965ebff
Remi Lecomte <remi.lecomte.etu>
2021-10-16 15:26:34 +0200

    ajout des comparateurs de points et de triangles

--- src/main/java/triangle/TriangleComparatorX.java
+++ src/main/java/triangle/TriangleComparatorX.java
@@ -1,22 +1,19 @@
 package triangle;
 
-import java.util.ArrayList;
 import java.util.Comparator;
 
-public class TriangleComparatorZ implements Comparator<Triangle>{
-
+public class TriangleComparatorX implements Comparator<Triangle>{
 	@Override
 	public int compare(Triangle t1, Triangle t2) {
 		Point[] points1=t1.getPoints();
 		Point[] points2=t2.getPoints();
 		for(int i=0; i<points1.length;i++) {
-			if(points1[i].getZ()>points2[i].getZ()) {
+			if(points1[i].getX()>points2[i].getX()) {
 				return 1;
-			}else if(points1[i].getZ()>points2[i].getZ()) {
+			}else if(points1[i].getX()>points2[i].getX()) {
 				return -1;
 			}
 		}
 		return 0;
 	}
-	
 }
