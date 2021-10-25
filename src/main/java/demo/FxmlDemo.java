16ba5f55eeda26eef0ed2aa5855e66f29686f06a
Hugo Roux <hugo.roux.etu@univ-lille.fr>
2021-10-25 17:19:27 +0200

    adding anchorPne and a few other assets

--- src/main/java/demo/FxmlDemo.java
+++ src/main/java/demo/FxmlDemo.java
@@ -1,27 +1,134 @@
+
 package demo;
 
 import java.io.IOException;
-
+/*
 import javafx.application.Application;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 import javafx.fxml.FXMLLoader;
+import javafx.scene.*;
+import javafx.scene.paint.*;
+import javafx.scene.canvas.*;
+*/
+
+import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Orientation;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ListView;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+import javafx.scene.*;
+import javafx.scene.paint.*;
+import javafx.scene.canvas.*;
+import javafx.scene.control.Slider;
 
 public class FxmlDemo extends Application {
-
+	
 	// java --module-path 'path-to-javafx-binaries/lib' --add-modules javafx.controls,javafx.fxml -jar .\projetmode.jar
 	public void start(Stage stage) throws IOException {
+		/*
             FXMLLoader loader = new FXMLLoader();
             loader.setLocation(getClass().getResource("/simpleFxmlDemo.fxml"));
             Parent root = loader.load();
-
+            
             Scene scene = new Scene(root);
             stage.setScene(scene);
             stage.setTitle("ProjetModeJ1");
             stage.setResizable(false);
             stage.show();
-    }
+            */
+			AnchorPane root = new AnchorPane();
+		       /*
+		        root.setSpacing(10);
+		        root.setPadding(new Insets(15,20, 10,10));
+		       */
+		        Book book1 = new Book(1L, "J01", "Java IO Tutorial");
+		        Book book2 = new Book(2L, "J02", "Java Enums Tutorial");
+		        Book book3 = new Book(2L, "C01", "C# Tutorial for Beginners");
+
+		        // To Creating a Observable List
+		        ObservableList<Book> books = FXCollections.observableArrayList(book1, book2, book3);
+
+		        // Create a ListView
+		        ListView<Book> listView = new ListView<Book>(books);
+
+		        // To set multiple selection model
+		        listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
+
+		        // Select item at index = 1,2
+		        listView.getSelectionModel().selectIndices(1, 2);
+
+		        // Focus
+		        listView.getFocusModel().focus(1);
+
+		        //StackPane root = new StackPane();
+		        root.getChildren().add(listView);
+		        
+		        // Button up
+		        Button button1 = new Button("/\\");
+		        button1.setPrefSize(10, 10);
+		        root.getChildren().add(button1);
+		       
+		       
+		        // Button bas
+		        Button button2 = new Button("\\/");
+		        button2.setPrefSize(10, 10);
+		        root.getChildren().add(button2);
+		        
+		        // Button de droite
+		        Button button3 = new Button(">");
+		        button3.setPrefSize(10, 10);
+		        root.getChildren().add(button3);
+		       
+		       
+		        // Button de gauche
+		        Button button4 = new Button("<");
+		        button4.setPrefSize(10, 10);
+		        root.getChildren().add(button4);
+		        
+		        //slider
+		        Slider slider = new Slider(0, 180, 0);
+		        slider.setOrientation(Orientation.VERTICAL);
+		        root.getChildren().add(slider);
+		       
+		        Canvas canvas = new Canvas(250,250);
+		        root.getChildren().add(canvas);
+		        
+		        // TextField
+		        TextField textField = new TextField("Text Field");
+		        textField.setPrefWidth(110);
+		         
+		       
+		        root.getChildren().add(textField);
+		       
+		        // CheckBox
+		        CheckBox checkBox = new CheckBox("Check Box");
+		         
+		        root.getChildren().add(checkBox);
+		       
+		        // RadioButton
+		        RadioButton radioButton = new RadioButton("Radio Button");
+		        root.getChildren().add(radioButton);
+		       
+		        Scene scene = new Scene(root, 640, 400);
+
+		        stage.setTitle("ProjetModeJ1");
+		        stage.setScene(scene);
+		        stage.setResizable(false);
+		        stage.show();
+		    }
 
     public static void main(String[] args) {
             Application.launch(args);
--- src/main/java/demo/ListViewDemo.java
+++ src/main/java/demo/ListViewDemo.java
@@ -1,66 +1,49 @@
 package demo;
 
-import java.util.ArrayList;
-import java.util.List;
 import javafx.application.Application;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
-import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.Priority;
+import javafx.scene.control.SelectionMode;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
-
 
 public class ListViewDemo extends Application {
 
-     public static class HBoxCell extends HBox {
-          Label label = new Label();
-          Button button = new Button();
+  @Override
+  public void start(Stage stage) {
 
-          HBoxCell(String labelText, String buttonText) {
-               super();
+      Book book1 = new Book(1L, "J01", "Java IO Tutorial");
+      Book book2 = new Book(2L, "J02", "Java Enums Tutorial");
+      Book book3 = new Book(2L, "C01", "C# Tutorial for Beginners");
 
-               label.setText(labelText);
-               label.setMaxWidth(Double.MAX_VALUE);
-               HBox.setHgrow(label, Priority.ALWAYS);
+      // To Creating a Observable List
+      ObservableList<Book> books = FXCollections.observableArrayList(book1, book2, book3);
 
-               button.setText(buttonText);
+      // Create a ListView
+      ListView<Book> listView = new ListView<Book>(books);
 
-               this.getChildren().addAll(label, button);
-          }
-     }
+      // To set multiple selection model
+      listView.getSelectionModel().setSelectionMode(SelectionMode.MULTIPLE);
 
-     public Parent createContent() {
-          BorderPane layout = new BorderPane();
+      // Select item at index = 1,2
+      listView.getSelectionModel().selectIndices(1, 2);
 
-          List<HBoxCell> list = new ArrayList<>();
-          for (int i = 0; i < 12; i++) {
-               list.add(new HBoxCell("Item " + i, "Button " + i));
-          }
+      // Focus
+      listView.getFocusModel().focus(1);
 
-          ListView<HBoxCell> listView = new ListView<HBoxCell>();
-          ObservableList<HBoxCell> myObservableList = FXCollections.observableList(list);
-          listView.setItems(myObservableList);
+      StackPane root = new StackPane();
+      root.getChildren().add(listView);
 
-          layout.setCenter(listView);
+      stage.setTitle("ListView (o7planning.org)");
 
-          return layout;
-     }
+      Scene scene = new Scene(root, 350, 200);
+      stage.setScene(scene);
+      stage.show();
+  }
 
-     @Override
-     public void start(Stage stage) throws Exception {
-          stage.setScene(new Scene(createContent()));
-          stage.setWidth(300);
-          stage.setHeight(200);
-          stage.show();
-     }
-
-     public static void main(String args[]) {
-          launch(args);
-     }
+  public static void main(String[] args) {
+      launch(args);
+  }
 }
