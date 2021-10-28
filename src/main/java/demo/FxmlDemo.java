72e61b8bdaaa595afd410aebc7921ac02f1cfd43
Hugo Roux <hugo.roux.etu@univ-lille.fr>
2021-10-28 11:28:51 +0200

    some new things

--- src/main/java/demo/FxmlDemo.java
+++ src/main/java/demo/FxmlDemo.java
@@ -27,10 +27,12 @@ import javafx.scene.control.SelectionMode;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
+import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.*;
 import javafx.scene.paint.*;
+import javafx.scene.shape.ArcType;
 import javafx.scene.canvas.*;
 import javafx.scene.control.Slider;
 
@@ -48,12 +50,12 @@ public class FxmlDemo extends Application {
             stage.setTitle("ProjetModeJ1");
             stage.setResizable(false);
             stage.show();
-            */
+            
 			AnchorPane root = new AnchorPane();
-		       /*
+		       
 		        root.setSpacing(10);
 		        root.setPadding(new Insets(15,20, 10,10));
-		       */
+		       
 		        Book book1 = new Book(1L, "J01", "Java IO Tutorial");
 		        Book book2 = new Book(2L, "J02", "Java Enums Tutorial");
 		        Book book3 = new Book(2L, "C01", "C# Tutorial for Beginners");
@@ -78,33 +80,50 @@ public class FxmlDemo extends Application {
 		        
 		        // Button up
 		        Button button1 = new Button("/\\");
-		        button1.setPrefSize(10, 10);
+		        button1.setPrefSize(20, 20);
 		        root.getChildren().add(button1);
+		        //AnchorPane.setTopAnchor(button1, 100.0);
+		        AnchorPane.setBottomAnchor(button1, 125.0);
+		        //AnchorPane.setLeftAnchor(button1, 100.0);
+		        AnchorPane.setRightAnchor(button1, 100.0);
 		       
 		       
 		        // Button bas
 		        Button button2 = new Button("\\/");
-		        button2.setPrefSize(10, 10);
+		        button2.setPrefSize(20, 20);
 		        root.getChildren().add(button2);
+		        AnchorPane.setBottomAnchor(button2, 75.0);
+		        AnchorPane.setRightAnchor(button2, 100.0);
 		        
 		        // Button de droite
 		        Button button3 = new Button(">");
-		        button3.setPrefSize(10, 10);
+		        button3.setPrefSize(20, 20);
 		        root.getChildren().add(button3);
+		        AnchorPane.setBottomAnchor(button3, 100.0);
+		        AnchorPane.setRightAnchor(button3, 73.0);
 		       
 		       
 		        // Button de gauche
 		        Button button4 = new Button("<");
-		        button4.setPrefSize(10, 10);
+		        button4.setPrefSize(20, 20);
 		        root.getChildren().add(button4);
+		        AnchorPane.setBottomAnchor(button4, 100.0);
+		        AnchorPane.setRightAnchor(button4, 125.0);
 		        
 		        //slider
 		        Slider slider = new Slider(0, 180, 0);
 		        slider.setOrientation(Orientation.VERTICAL);
 		        root.getChildren().add(slider);
+		        AnchorPane.setTopAnchor(slider, 100.0);
+		        AnchorPane.setRightAnchor(slider, 125.0);
 		       
 		        Canvas canvas = new Canvas(250,250);
+		        GraphicsContext gc = canvas.getGraphicsContext2D();
+		        //drawShapes(gc);
+		        canvas.setStyle("-fx-background-color: red");
 		        root.getChildren().add(canvas);
+		        //AnchorPane.setTopAnchor(canvas, 100.0);
+		        //AnchorPane.setRightAnchor(canvas, 100.0);
 		        
 		        // TextField
 		        TextField textField = new TextField("Text Field");
@@ -122,14 +141,60 @@ public class FxmlDemo extends Application {
 		        RadioButton radioButton = new RadioButton("Radio Button");
 		        root.getChildren().add(radioButton);
 		       
-		        Scene scene = new Scene(root, 640, 400);
+		        Scene scene = new Scene(root, 1280, 800);
 
 		        stage.setTitle("ProjetModeJ1");
 		        stage.setScene(scene);
 		        stage.setResizable(false);
 		        stage.show();
+		        */
+				Pane root = new Pane();
+		
+		        StackPane holder = new StackPane();
+		        Canvas canvas = new Canvas(400,  300);
+		        canvas.setLayoutX(100);
+		        canvas.setLayoutY(400);
+		        //slider
+		        Slider slider = new Slider(0, 180, 0);
+		        slider.setOrientation(Orientation.VERTICAL);
+		        slider.setLayoutX(100);
+		        slider.setLayoutY(100);
+		       
+		
+		        holder.getChildren().add(canvas);
+		        root.getChildren().add(holder);
+		        root.getChildren().add(slider);
+		
+		        holder.setStyle("-fx-background-color: red");
+		        Scene scene = new Scene(root, 1280, 800);
+		        stage.setScene(scene);
+		        stage.show();
 		    }
-
+/*
+	private void drawShapes(GraphicsContext gc) {
+        gc.setFill(Color.GREEN);
+        gc.setStroke(Color.BLUE);
+        gc.setLineWidth(5);
+        gc.strokeLine(40, 10, 10, 40);
+        gc.fillOval(10, 60, 30, 30);
+        gc.strokeOval(60, 60, 30, 30);
+        gc.fillRoundRect(110, 60, 30, 30, 10, 10);
+        gc.strokeRoundRect(160, 60, 30, 30, 10, 10);
+        gc.fillArc(10, 110, 30, 30, 45, 240, ArcType.OPEN);
+        gc.fillArc(60, 110, 30, 30, 45, 240, ArcType.CHORD);
+        gc.fillArc(110, 110, 30, 30, 45, 240, ArcType.ROUND);
+        gc.strokeArc(10, 160, 30, 30, 45, 240, ArcType.OPEN);
+        gc.strokeArc(60, 160, 30, 30, 45, 240, ArcType.CHORD);
+        gc.strokeArc(110, 160, 30, 30, 45, 240, ArcType.ROUND);
+        gc.fillPolygon(new double[]{10, 40, 10, 40},
+                       new double[]{210, 210, 240, 240}, 4);
+        gc.strokePolygon(new double[]{60, 90, 60, 90},
+                         new double[]{210, 210, 240, 240}, 4);
+        gc.strokePolyline(new double[]{110, 140, 110, 140},
+                          new double[]{210, 210, 240, 240}, 4);
+    }
+    */
+	
     public static void main(String[] args) {
             Application.launch(args);
     }
