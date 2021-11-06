e02c615e9ca53bb6594bf42c4113b228ff7c7a89
Remi Lecomte <remi.lecomte.etu>
2021-11-06 10:56:01 +0100

    update

--- src/test/java/repereTest/RepereTest.java
+++ src/test/java/repereTest/RepereTest.java
@@ -15,7 +15,7 @@ class RepereTest {
 		Repere repere=new Repere();
 		repere.turnOnYAxisOf(90);
 		
-		assertArrayEquals(repere.matrice, new double[][] {{0,0,-1,0},
+		assertArrayEquals(repere.matrice.points, new double[][] {{0,0,-1,0},
 														  {0,1,0,0},
 														  {1,0,0,0},
 														  {0,0,0,1}
@@ -23,7 +23,7 @@ class RepereTest {
 
 		repere.turnOnYAxisOf(-90);
 		repere.turnOnXAxisOf(180);
-		assertArrayEquals(repere.matrice, new double[][] {{1,0,0,0},
+		assertArrayEquals(repere.matrice.points, new double[][] {{1,0,0,0},
 														  {0,-1,0,0},
 														  {0,0,-1,0},
 														  {0,0,0,1}
@@ -33,7 +33,7 @@ class RepereTest {
 		repere.turnOnYAxisOf(180);
 		repere.turnOnXAxisOf(270);
 		
-		assertArrayEquals(repere.matrice, new double[][] {{-1,0,0,0},
+		assertArrayEquals(repere.matrice.points, new double[][] {{-1,0,0,0},
 														  {0,0,-1,0},
 														  {0,-1,0,0},
 														  {0,0,0,1}
@@ -42,11 +42,11 @@ class RepereTest {
 		repere.turnOnYAxisOf(45);
 		double arrondiSqrtDeux=(double)Math.round((Math.sqrt(2)*10000))/10000;
 		System.out.println(arrondiSqrtDeux/2);
-		System.out.println("\n--------\n"+Arrays.toString(repere.matrice[0]));
-		System.out.println(Arrays.toString(repere.matrice[1]));
-		System.out.println(Arrays.toString(repere.matrice[2]));
-		System.out.println(Arrays.toString(repere.matrice[3]));
-		assertArrayEquals(repere.matrice, new double[][] {{arrondiSqrtDeux/2,0,-1*arrondiSqrtDeux/2,0},
+		System.out.println("\n--------\n"+Arrays.toString(repere.matrice.points[0]));
+		System.out.println(Arrays.toString(repere.matrice.points[1]));
+		System.out.println(Arrays.toString(repere.matrice.points[2]));
+		System.out.println(Arrays.toString(repere.matrice.points[3]));
+		assertArrayEquals(repere.matrice.points, new double[][] {{arrondiSqrtDeux/2,0,-1*arrondiSqrtDeux/2,0},
 			  {0,1,0,0},
 			  {arrondiSqrtDeux/2,0,arrondiSqrtDeux/2,0},
 			  {0,0,0,1}
@@ -59,7 +59,7 @@ class RepereTest {
 
 		Repere repere=new Repere();
 		repere.translation(5, 10, 3);
-		assertArrayEquals(repere.matrice, new double[][] {{1,0,0,5},
+		assertArrayEquals(repere.matrice.points, new double[][] {{1,0,0,5},
 														  {0,1,0,10},
 														  {0,0,1,3},
 														  {0,0,0,1}});
@@ -69,19 +69,24 @@ class RepereTest {
 
 		Repere repere=new Repere();
 		repere.homotetie(0.5);
-		assertArrayEquals(repere.matrice, new double[][] {{0.5,0,0,0},
+		assertArrayEquals(repere.matrice.points, new double[][] {{0.5,0,0,0},
 														  {0,0.5,0,0},
 														  {0,0,0.5,0},
 														  {0,0,0,1}});
 		repere=new Repere();
 		repere.translation(2, 1, 0);
+
+		System.out.println("\n--------\n"+Arrays.toString(repere.matrice.points[0]));
+		System.out.println(Arrays.toString(repere.matrice.points[1]));
+		System.out.println(Arrays.toString(repere.matrice.points[2]));
+		System.out.println(Arrays.toString(repere.matrice.points[3]));
 		repere.homotetie(2);
-		System.out.println("\n--------\n"+Arrays.toString(repere.matrice[0]));
-		System.out.println(Arrays.toString(repere.matrice[1]));
-		System.out.println(Arrays.toString(repere.matrice[2]));
-		System.out.println(Arrays.toString(repere.matrice[3]));
-		assertArrayEquals(repere.matrice, new double[][] {{2,0,0,2},
-			  {0,2,0,1},
+		System.out.println("\n--------\n"+Arrays.toString(repere.matrice.points[0]));
+		System.out.println(Arrays.toString(repere.matrice.points[1]));
+		System.out.println(Arrays.toString(repere.matrice.points[2]));
+		System.out.println(Arrays.toString(repere.matrice.points[3]));
+		assertArrayEquals(repere.matrice.points, new double[][] {{2,0,0,4},
+			  {0,2,0,2},
 			  {0,0,2,0},
 			  {0,0,0,1}});
 	}
